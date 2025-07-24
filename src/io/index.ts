7961dec0102f56293101b5be816f4b8bae6b17f9
ZO CD <zoverhulser@gmail.com>
2025-07-24 01:16:43 +0000

    fixes double-export

--- src/io/index.ts
+++ src/io/index.ts
@@ -1,5 +1,4 @@
 // I/O operations
 export { convertText, processFile } from '../processors/file-processor'
 export { processDirectory } from '../processors/directory-processor'
-export { ensureDirExists } from '../utils/file-utils'
 export * from '../transformations/text-to-lines'
